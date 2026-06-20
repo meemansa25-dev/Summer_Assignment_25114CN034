cbbd182ffb5494d043bc9bffeedc127bfd71fcb2
Meemansa Dwivedi <meemansadwivedi25@gmail.com>
2026-06-20 12:21:53 +0530

    Add Day-20 folder

---
+++ Day-20/1.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int r1, c1, r2, c2;
+
+    cin >> r1 >> c1;
+    cin >> r2 >> c2;
+
+    if(c1 != r2) {
+        cout << "Multiplication Not Possible";
+        return 0;
+    }
+
+    int a[r1][c1], b[r2][c2], result[r1][c2];
+
+    for(int i = 0; i < r1; i++)
+        for(int j = 0; j < c1; j++)
+            cin >> a[i][j];
+
+    for(int i = 0; i < r2; i++)
+        for(int j = 0; j < c2; j++)
+            cin >> b[i][j];
+
+    for(int i = 0; i < r1; i++) {
+        for(int j = 0; j < c2; j++) {
+            result[i][j] = 0;
+
+            for(int k = 0; k < c1; k++)
+                result[i][j] += a[i][k] * b[k][j];
+        }
+    }
+
+    for(int i = 0; i < r1; i++) {
+        for(int j = 0; j < c2; j++)
+            cout << result[i][j] << " ";
+
+        cout << endl;
+    }
+
+    return 0;
+}
