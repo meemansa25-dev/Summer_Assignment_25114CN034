c82d8f9cd36f967b340a9ce3f2ae6759a17a5cd9
Meemansa Dwivedi <meemansadwivedi25@gmail.com>
2026-06-29 13:39:01 +0530

    Adding Day-29 folder

---
+++ Day-29/3.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string str;
+
+    getline(cin, str);
+
+    cout << "Length = " << str.length();
+
+    return 0;
+}
---
+++ Day-29/4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string item;
+    int quantity;
+
+    cin >> item;
+    cin >> quantity;
+
+    cout << "Item: " << item << endl;
+    cout << "Quantity: " << quantity;
+
+    return 0;
+}
